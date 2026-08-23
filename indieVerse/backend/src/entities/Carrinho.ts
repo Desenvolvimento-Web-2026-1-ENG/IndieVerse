02a09428bbec9086fcb937005951a62334456f8d
Pedro Henrique de Souza Seabra Mattos Cordeiro <69598019+PHCordeiro@users.noreply.github.com>
2026-08-23 11:34:26 -0300

    Alteração do Checkout para PUT e criação de status na entidade do carrinho

--- indieVerse/backend/src/entities/Carrinho.ts
+++ indieVerse/backend/src/entities/Carrinho.ts
@@ -1,4 +1,12 @@
+export type StatusCarrinho = "ABERTO" | "FINALIZADO" | "CANCELADO";
+
 export interface ItemCarrinho {
   jogadorId: number;
   jogoId: number;
+}
+
+export interface Carrinho {
+  jogadorId: number;
+  status: StatusCarrinho;
+  itens: ItemCarrinho[];
 }
